1ac4acf44900f1d70e1d19841d24b367a0dcdc01
Oleg20502 <oleg20502@gmail.com>
2022-05-12 14:50:45 +0300

    Created python

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,15 +11,17 @@
 TOKEN='5232246878:AAH2F0ahSUavW9Q_-ufAQpTHf4m2zPTf_ao'
 CHATID='890953180'
 
-crontab crontab.sh
 cd ~/NaCl-EIM
+crontab scripts/crontab.sh
 
 mkdir -p log/
 mkdir -p rdf/
-mkdir -p msd/
+mkdir -p dump/
 mkdir -p png/
 
-for temp in 1000
+curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d text="Started"
+
+for temp in 600 700 800 900 1000 1100 1200 1300 1400 1500 1600
 do
 	sed "s/YYYYTEMP/$temp/g" input/in.eim_raw > in.eim_for_run
 	srun --exclusive -N 1 -p RT --ntasks-per-node=8 ~/bin/lmp_mpi -in in.eim_for_run > log/log.lammps_$temp
